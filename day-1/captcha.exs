043028d3c264acb4a81143eb0bb44df8ab3e1a47
Matthew King <mattking73@gmail.com>
2017-12-13 21:56:24 -0800

    implement check_half..probably can be cleaner

--- day-1/captcha.exs
+++ day-1/captcha.exs
@@ -1,16 +1,33 @@
 defmodule Captcha do
-    def check([head | tail]) do
-        check([head] ++ tail ++ [head], 0)
-    end
-    def check(number) do
+    def to_array(number) do
         number
         |> Integer.digits()
-        |> check()
+    end
+
+    def check([head | tail]) do
+        check([head] ++ tail ++ [head], 0)
     end
 
     def check([], _), do: 0
     def check([head | tail], last) when head == last, do: head + check(tail, head)
     def check([head | tail], _), do: check(tail, head)
+
+    def check_half(array) do
+        array_half = div length(array), 2
+        [first_chunk | _] = Enum.chunk(array, array_half)
+        check_half(array ++ first_chunk, array_half, 0)
+    end
+
+    def check_half(array, array_half, acc) when array_half > length(array) do
+        acc
+    end
+    def check_half([head | tail], array_half, acc) do
+        if head == Enum.at(tail, array_half - 1) do
+            head + check_half(tail, array_half, acc)
+        else
+            check_half(tail, array_half, acc)
+        end
+    end
 end
 
 # Testers
@@ -22,5 +39,7 @@ end
 IO.gets("Enter captcha input: ")
 |> String.slice(0..-2)
 |> String.to_integer()
-|> Captcha.check()
+|> Captcha.to_array()
+# |> Captcha.check()
+|> Captcha.check_half()
 |> IO.puts
