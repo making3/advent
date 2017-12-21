f4192d3e72bb56abfc518a17a6d1313e7483700c
Matthew King <mattking73@gmail.com>
2017-12-21 15:00:26 -0800

    Implement 'complex' version, reddit helped out with speed stuff

--- day-5/jumps.exs
+++ day-5/jumps.exs
@@ -1,28 +1,39 @@
 defmodule Jump do
   def get_steps(file) do
     File.stream!(file)
-    |> Stream.map(&(String.trim(&1, "\n") |> String.to_integer()))
-    |> Enum.to_list()
+    |> Enum.map(&(String.trim(&1, "\n") |> String.to_integer()))
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, fn {jump, i}, jumps ->
+      Map.put(jumps, i, jump)
+    end)
   end
 
   def steps([]), do: 0
-  def steps(list) do
-    step(list, 0, 0)
+  def steps(list, alter_step_length) do
+    step(list, alter_step_length, 0, 0)
   end
 
-  defp step(list, index, step_count) when index + 1 > length(list) do
+  defp step(list, _, index, step_count) when index + 1 > map_size(list) do
     step_count
   end
-  defp step(list, index, step_count) do
-    step_length = Enum.fetch!(list, index)
+  defp step(list, alter_step_length, index, step_count) do
+    step_length = list[index]
 
     list
-    |> List.replace_at(index, step_length + 1)
-    |> step(index + step_length, step_count + 1)
+    |> Map.put(index, alter_step_length.(list[index]))
+    |> step(alter_step_length, index + step_length, step_count + 1)
   end
+
+  def alter_length_simple(step_length), do: step_length + 1
+
+  def alter_length_complex(step_length) when step_length > 2, do: step_length - 1
+  def alter_length_complex(step_length), do: step_length + 1
 end
 
-# Test
-# Jump.steps([0, 3, 0, 1, -3]) |> IO.puts() # 5
+# Test Part 1
+# Jump.steps([0, 3, 0, 1, -3], &Jump.alter_length_simple/1) |> IO.puts() # 5
 
-Jump.get_steps('input') |> Jump.steps() |> IO.puts()
+# Test Part 2
+# Jump.steps([0, 3, 0, 1, -3], &Jump.alter_length_complex/1) |> IO.puts() # 5
+
+Jump.get_steps('input') |> Jump.steps(&Jump.alter_length_complex/1) |> IO.puts()
