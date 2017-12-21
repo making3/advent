8f4451afe15fba883ebeb2ba05a083c9e6490103
Matthew King <mattking73@gmail.com>
2017-12-21 13:58:26 -0800

    add day 4 part 2

--- day-4/passphrase.exs
+++ day-4/passphrase.exs
@@ -1,18 +1,37 @@
 defmodule Passphrase do
-  def is_valid(phrase) do
+  def is_valid_simple(phrase) do
     words = String.split(phrase, " ")
     set = MapSet.new(words)
     MapSet.size(set) == length(words)
   end
 
-  def count(file) do
+  def is_valid_secure(phrase) do
+    words = get_words(phrase)
+    set = MapSet.new(words)
+    MapSet.size(set) == length(words)
+  end
+
+  defp get_words(phrase) do
+    words = phrase
+    |> String.split(" ")
+    |> Enum.map(&sort_word/1)
+  end
+  
+  defp sort_word(word) do
+    word
+    |> String.to_charlist()
+    |> Enum.sort()
+    |> to_string()
+  end
+
+  def count(file, is_valid) do
     file
     |> File.open!()
     |> IO.stream(:line)
     |> Enum.reduce(0, fn(line, acc) ->
       line
       |> String.trim("\n")
-      |> is_valid()
+      |> is_valid.()
       |> reduce(acc)
     end)
   end
@@ -27,4 +46,16 @@ end
 #Passphrase.is_valid("aa bb cc dd aaa") |> IO.puts()
 
 # Part 1
-Passphrase.count('input') |> IO.puts()
+#Passphrase.count('input', &Passphrase.is_valid_simple/1) |> IO.puts()
+
+
+# Tests 2
+#Passphrase.is_valid_secure("abcde fghij") |> IO.puts() # True
+#Passphrase.is_valid_secure("a ab abc abd abf abj") |> IO.puts() # True
+#Passphrase.is_valid_secure("iiii oiii ooii oooi oooo") |> IO.puts() # True
+#IO.puts("")
+#Passphrase.is_valid_secure("abcde xyz ecdab") |> IO.puts() # False
+#Passphrase.is_valid_secure("oiii ioii iioi iiio") |> IO.puts() # False
+
+# Part 2
+Passphrase.count('input', &Passphrase.is_valid_secure/1) |> IO.puts()
